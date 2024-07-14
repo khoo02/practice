fc645b6addcc4477e6a71571b149b13914273c21
khoo02 <khooiverson02@gmail.com>
2024-07-14 05:56:14 -0700

    create basic js functions

---
+++ js/practice_4/script.js
@@ -0,0 +1,14 @@
+// addition
+let add7 = (num) => num+7;
+// multiplication
+function multiply(num1, num2) { 
+    return num1*num2;
+}
+// capitalize or lowercase first letter only
+function capitalize(string) {
+    if (string[0] !== string[0].toUpperCase()) {
+        return string[0].toUpperCase() + string.slice(1);
+    } else {
+        return string[0].toLowerCase() + string.slice(1);
+    }
+}
