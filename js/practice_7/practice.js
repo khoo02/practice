66f8c7a6d85b9445bbc5fb9bddb3f0b4a00052cd
khoo02 <khooiverson02@gmail.com>
2025-03-19 16:30:03 -0700

    practice DOM manipulation

---
+++ js/practice_7/practice.js
@@ -0,0 +1,37 @@
+const container = document.querySelector("#container");
+
+const contentP = document.createElement("p");
+
+    contentP.classList.add("contentP");
+
+    contentP.textContent = "Hey I'm red!";
+
+    contentP.style.color = "red";
+
+    container.appendChild(contentP);
+
+const contentH3 = document.createElement("h3");
+
+    contentH3.classList.add("contentH3");
+
+    contentH3.textContent = "I'm a blue h3!";
+
+    contentH3.style.color = "blue";
+
+    container.appendChild(contentH3);
+
+const contentDiv = document.createElement("div");
+
+    const divH1 = document.createElement("h1");
+
+        divH1.textContent = "I'm in a div";
+
+        contentDiv.appendChild(divH1);
+
+    const divP = document.createElement("p")
+
+        divP.textContent = "ME TOO!";
+
+        contentDiv.appendChild(divP);
+
+    container.appendChild(contentDiv);
